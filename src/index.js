2c982efa02fe82c87c67e9ae7dd8bd9a68d5ffa0
danielstrolle <danielstrolle@gmail.com>
2017-09-13 18:01:22 -0500

    Add functionality to add buttons when getMovieList is called
    *minor css updating
    *clean up db, was using static variable

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 const movieAPI = require('./getMovies.js');
 
-movieAPI.getMovies().then((movies) => {
-  let movieApp = document.getElementById("movieApp");
-  let movieAppHTML = "<dl>";
-  movies.forEach((movie) => {
-      movieAppHTML += `<dt>${movie.title}</dt><dd>Rating: `
-    for (let i =0; i < movie.rating; i++) {
-        movieAppHTML += '&#9733;';
-      } movieAppHTML += '</dd>';
-  });
-  movieAppHTML += "</dl>";
-  movieApp.innerHTML = movieAppHTML;
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-});
+function getMovieList() {
+    movieAPI.getMovies().then((movies) => {
+        let movieApp = document.getElementById("movieApp");
+        let movieAppHTML = "<dl>";
+        movies.forEach((movie) => {
+            movieAppHTML += `<dt>${movie.title}</dt><dd>Rating: `
+            for (let i = 0; i < movie.rating; i++) {
+                movieAppHTML += '&#9733;';
+            }
+            movieAppHTML += '</dd>';
+        });
+        movieAppHTML += "</dl>";
+        movieApp.innerHTML = movieAppHTML;
+        document.getElementById("buttons").innerHTML = `
+        <button id="addButton">Add New Movie</button>
+        <button>Edit Existing Movie</button>
+        <button>Delete Movie</button>`;
+        document.getElementById("addButton").addEventListener("click", addNewMovie);
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+    });
+}
 
-movieAPI.addMovie()
-    .then(res => res.json().then((movie) => console.log(movie)));
+function addNewMovie () {
+    alert("add movie");
+}
+
+getMovieList();
+// movieAPI.addMovie()
+//     .then(res => res.json().then((movie) => console.log(movie)));
